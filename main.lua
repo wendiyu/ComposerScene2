aeae54a25fa181935705b5c946352248ac4d8be9
Wendi Yu <s439178@ocsb.local>
2018-05-16 12:23:16 -0400

    update

--- main.lua
+++ main.lua
@@ -2,6 +2,16 @@
 --
 -- main.lua
 --
+-- Created by: Wendi Yu
+-- Created on: May 2018
+-- 
+-- This file is create a composer
 -----------------------------------------------------------------------------------------
 
--- Your code here
+-- composer variable
+local composer = require( "composer" )
+
+display.setStatusBar(display.HiddenStatusBar)
+
+composer.gotoScene( "splashScene")
+ 
